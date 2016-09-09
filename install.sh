e493b4c2778597d4dcc2b83f0e10642cbb4fb770
Nick Stogner <nicholas.stogner@gmail.com>
2016-09-09 11:56:12 -0400

    Fix coloring

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 ln -s ~/.spf13-vim.local/.vimrc.local ~/.vimrc.local
 ln -s ~/.spf13-vim.local/.vimrc.before.local ~/.vimrc.before.local
 
+# Install colors
+mkdir -p ~/.vim/colors
+cp ~/.spf13-vim.local/colors/* ~/.vim/colors
+
